86a2fc6c55008472b2075910846156cb549e5e9d
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2024-07-16 10:20:18 -0800

    Removed TCP connector dependencies in Unity

---
+++ Assets/Info/ros_setup_info.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ros_setup_info : MonoBehaviour
+{
+
+    // ros2 run ros_tcp_endpoint default_server_endpoint
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
